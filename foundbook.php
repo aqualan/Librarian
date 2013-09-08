406e5df5278d9301a7ad0da918ff35834534e5aa
Andrew Harper <graphitezeppelin@gmail.com>
2013-09-08 01:45:38 -0400

    woooOWOooOOOoOoOOOoO it sucks

---
+++ foundbook.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+	<title>Librarian</title>
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<link href='http://fonts.googleapis.com/css?family=Arvo' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+<!--link rel="stylesheet" type="text/css" href="css/style.css"/-->
+<link rel="stylesheet" type="text/css" href="css/pennapps.css">	
+</head>
+
+<body>
+
+
+	<div class="container">
+		<div class="white_descript_h" id="welcome_search">
+			
+			<p>Your book is on the third floor <?php echo $_GET["rownum"]; ?> , 16 shelves down.</p>
+		</div>		
+	</div>
+
+
+</html>
